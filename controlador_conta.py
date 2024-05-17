9438f0b29f08269efd8a6e65de42f3d095a4abdb
artlemes <arthur.c.lemes@gmail.com>
2024-05-17 13:33:38 -0300

    começando a tratar conta e mvc
    criei a fundação do controlador da conta e criei completamente a tela da conta

--- controlador_conta.py
+++ controlador_conta.py
@@ -1,7 +1,10 @@
 from tela_conta import TelaConta
+
+
 class ControladorConta():
     def __init__(self):
         self.__tela_conta = TelaConta()
+        self.__contas_pagas = []
         self.__contas = []
 
     @property
@@ -12,27 +15,70 @@ class ControladorConta():
     def contas(self):
         return self.__contas
     
-    def adiciona_conta(self):
-        conta_dados = self.tela_conta.pega_dados_conta()
+    @property
+    def contas_pagas(self):
+        return self.__contas_pagas
+    
+    def criar_conta(self):
+        pass
 
-    #aqui eu acho que da pra aceitar pedido repetido, desde que nao seja o mesmo codigo
-    #a pessoa pode pedir dois refris iguais 
     def adiciona_produto(self):
         self.lista_contas()    
         self.escolhe_conta()
     
-    """ def remove_produto(self):
+    def remover_produto(self):
+        pass
 
-    #aqui eu penso que temos que listar as contas e pedidos
     def lista_contas(self):
+        pass
 
-    def escolhe_conta(self):
+    def pagar_conta(self):
+        pass
 
-    def pagar_conta(self): 
+    def adicionar_produto(self):
+        pass
 
+    def listar_produtos_de_uma_conta(self):
+        pass
 
-    #aqui vamos automatizar do jeito que tu falou
-    #tu acha melhor numeros aleatorios ou uma ordem?
+    def mostrar_contas(self):
+        pass
+
+    def mostrar_contas_pagas(self):
+        pass
+
+    def deletar_conta(self):
+        pass
+
     def criar_conta(self):
-    
-"""
+        pass
+
+    #status: feito
+    def abre_tela_inicial(self):
+        continua = True
+        while continua:
+            try:
+                op = int(self.tela_conta.tela_opcoes())
+            except:
+                self.tela_conta.mostra_msg("opção não é um inteiro")
+                op = None
+            if op == 1:
+                self.criar_conta()
+            elif op == 2:
+                self.adicionar_produto()
+            elif op == 3:
+                self.remover_produto()
+            elif op == 4:
+                self.listar_produtos_de_uma_conta()
+            elif op == 5:
+                self.pagar_conta()
+            elif op == 6:
+                self.mostrar_contas()
+            elif op == 7:
+                self.mostrar_contas_pagas()
+            elif op == 8:
+                self.deletar_conta()
+            elif op == 0:
+                continua = False
+            else: 
+                self.tela_contato.mostra_msg("opção inválida")
--- tela_conta.py
+++ tela_conta.py
@@ -6,14 +6,53 @@ class TelaConta():
         print('-----Conta-----')
         print('Opcoes:')
         print('1) Criar conta')
-        print('2) Adicionar um produto')
-        print('4) Remover Produto')
-        print('5) Pagar a conta')
-        print('6) Mostrar contas')
-        print('7) Deletar conta')
+        print('2) Adicionar um produto') #listar os produtos disponiveis antes de adicionar
+        print('3) Remover um produto')
+        print('4) Listar produtos de uma conta')
+        print('5) Pagar a conta') 
+        print('6) Mostrar contas') 
+        print('7) Mostrar contas pagas') 
+        print('8) Deletar conta')
         print('0) Retornar')
 
         opcao = input('Escolha uma opcao:')
         return opcao
+    
+    def mostra_produto(self, dados_produto, produto):
+
+        if isinstance(produto, Prato):
+            print('Nome do prato: ', {dados_produto['nome']})
+            print('Preço do prato: ', {dados_produto['preco']})
+            print('Despesa do prato: ', {dados_produto['despesa']})
+            print('Código do prato: ', {dados_produto['codigo']})
+            print('\n')
+
+        elif isinstance(produto, Bebida):
+            print('Nome da bebida: ', {dados_produto['nome']})
+            print('Preço da bebida: ', {dados_produto['preco']})
+            print('Despesa da bebida: ', {dados_produto['despesa']})
+            print('Código da bebida: ', {dados_produto['codigo']})
+            print('\n')
+    
+    def mostra_conta(self, dados_conta):
+        print('Código da conta: ', {dados_conta['codigo']})
+        print('A conta foi paga? ', {dados_conta['pago']})
+        print('Valor total: ', {dados_conta['valor_total']})
+        print('Despesas totais: ', {dados_conta['despesa_total']})
+        print('\n')
+
+    def seleciona_conta(self):
+        cod = int(input('Digite o código da conta: '))
+
+        return cod
+    
+    def seleciona_produto(self):
+        cod = int(input('Digite o código do produto: '))
+
+        return cod
+    
+    def mostra_msg(self, msg):
+        print(msg)
+
     
     
